1004468f620b477196a44f2784df9161ce5ee604
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-25 00:24:54 +0200

    Update sw-register.js

--- sw-register.js
+++ sw-register.js
@@ -1,36 +1,23 @@
-// sw-register.js
 (function(global) {
   'use strict';
 
   const SWLib = {};
 
-  /**
-   * Registreer de Service Worker
-   * @param {string} swPath - pad naar de SW
-   */
   SWLib.register = function(swPath = '/sw.js') {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register(swPath)
-        .then(reg => {
-          console.log(`Service Worker geregistreerd: ${reg.scope}`);
-        })
-        .catch(err => {
-          console.error('SW registratie mislukt:', err);
-        });
+        .then(reg => console.log('Service Worker geregistreerd:', reg.scope))
+        .catch(err => console.error('SW registratie mislukt:', err));
     } else {
       console.warn('Service Workers niet ondersteund.');
     }
   };
 
-  /**
-   * Check of SW actief is
-   * @returns {boolean}
-   */
   SWLib.isActive = function() {
-    return navigator.serviceWorker && navigator.serviceWorker.controller ? true : false;
+    return !!(navigator.serviceWorker && navigator.serviceWorker.controller);
   };
 
-  // Auto-register bij DOMContentLoaded
+  // Auto-register
   if (document.readyState === 'complete' || document.readyState === 'interactive') {
     SWLib.register();
   } else {
